564be063efae3bc31018ffb0e94d417ea131c5cb
alecarlosjesus <profalexandre.jesus@fiap.com.br>
2024-09-06 09:26:05 -0300

    Utilização massiva de hooks, useState, useEffect, useNavigate e api fetch com promise. Realizando redirect.

--- aula09/src/routes/Home/index.tsx
+++ aula09/src/routes/Home/index.tsx
@@ -1,16 +1,64 @@
-import { Link } from "react-router-dom";
-
+import { useEffect, useState } from "react";
 
 export default function Home(){
 
     //MUDANDO O TÍTULO DA PÁGINA!!!
     document.title = "Home";
 
+    useEffect(() => {
+      console.log("Executando sempre!!!"); 
+    });
+
+    const [usuario, setUsuario] = useState({
+      id:0,
+      login:"",
+      avatar_url:""
+    })
+    
+    useEffect(() => {
+      fetch("https://api.github.com/users/felipe-2833")
+      .then(response =>{
+        if(!response.ok){
+            throw new Error("Requisição de dados falhou!!");
+          }
+          return response.json();
+        })
+        .then(data => {
+          console.log(data);
+          setUsuario(data);
+        })
+        .catch(error => {
+          console.error(error);
+        });
+
+    },[]);
+
+      let str = "";
+      const [contador , setContador] = useState(0);
+
+      const [conteudo] = useState("...")
+
+      const mostraDif = () => {
+        str = conteudo;
+      }
+
     return(
       <div>
         <h1>Olá, mundo sou o Home!</h1>
         <p>Este o componente da página principal...</p>
-        <p><Link to="/editar/produtos/1500">Editar Produto - 1500</Link></p>
+
+          <div>
+            <button onClick={()=> setContador(contador + 1)}>Contador - {contador}</button>
+            <button onClick={()=> mostraDif()}>Show Dif</button>
+              <p>{conteudo}</p>
+          </div>
+          <div>
+            <figure>
+              <img src={usuario.avatar_url} alt={usuario.login} />
+              <figure>{usuario.login}</figure>
+            </figure>
+          </div>
+
       </div>
     );
   }
