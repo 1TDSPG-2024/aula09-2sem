2343f20f6a4a7511d92aa37d50e3b4f408d79ad0
victoraugustogfavaro <victoraugustogfavaro@gmail.com>
2024-09-06 09:51:52 -0300

    Hooks e Fetch (API)

--- aula09/src/routes/Home/index.tsx
+++ aula09/src/routes/Home/index.tsx
@@ -1,16 +1,67 @@
-import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
+export default function Home() {
+  //MUDANDO O TÍTULO DA PÁGINA!!!
+  document.title = "Home";
 
-export default function Home(){
+  const navigate = useNavigate();
 
-    //MUDANDO O TÍTULO DA PÁGINA!!!
-    document.title = "Home";
+  useEffect(() => {
+    console.log("Executando sempre!!!");
+  });
 
-    return(
+  const [usuario, setUsuario] = useState({
+    id: 0,
+    login: "",
+    avatar_url: "",
+  });
+
+  useEffect(() => {
+    fetch("https://api.github.com/users/victoraugustogfavaro")
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Requisição de dados falhou!!");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        console.log(data);
+        setUsuario(data);
+      })
+      .catch((error) => {
+        console.error(error);
+        navigate("/error");
+      });
+  }, []);
+
+  let str = "";
+  const [contador, setContador] = useState(0);
+
+  const [conteudo] = useState("...");
+
+  const mostraDif = () => {
+    str = conteudo;
+  };
+
+  return (
+    <div>
+      <h1>Olá, mundo sou o Home!</h1>
+      <p>Este o componente da página principal...</p>
+
       <div>
-        <h1>Olá, mundo sou o Home!</h1>
-        <p>Este o componente da página principal...</p>
-        <p><Link to="/editar/produtos/1500">Editar Produto - 1500</Link></p>
+        <button onClick={() => setContador(contador + 1)}>
+          Contador - {contador}
+        </button>
+        <button onClick={() => mostraDif()}>Show Dif</button>
+        <p>{conteudo}</p>
       </div>
-    );
-  }
+      <div>
+        <figure>
+          <img src={usuario.avatar_url} alt={usuario.login} />
+          <figure>{usuario.login}</figure>
+        </figure>
+      </div>
+    </div>
+  );
+}
